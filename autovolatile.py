c03c16f3127c3aa567d75f02c0c415840f5d1689
Cullen Rezendes <cxr5971@rit.edu>
2021-04-27 21:44:06 -0400

    Ready to test finding profiles

--- autovolatile.py
+++ autovolatile.py
@@ -1,12 +1,13 @@
 import argparse
 import subprocess
+from vol_bot import Vol_Bot
 
-def execute_pslist(memory_file, profile):
-    pslist_output = subprocess.run(["volatility", profile,], capture_output=True, text=True)
+def execute_pslist(vol_engine):
+    #pslist_output = subprocess.run(["volatility", ], capture_output=True, text=True)
+    return
 
 
-
-def execute_psscan(memory_file):
+def execute_psscan(vol_engine):
     return
 
 
@@ -15,7 +16,7 @@ def execute_psscan(memory_file):
 
 
 
-def execute_psxview(memory_file):
+def execute_psxview(vol_engine):
     return
 
 
@@ -23,37 +24,26 @@ def execute_psxview(memory_file):
 
 
 #
-def execute_process_finder(memory_file):
+def execute_process_finder(vol_engine):
     return
 
 
-def determine_profile(memory_file):
-    profile_output = subprocess.run(["volatility", memory_file, "kdbgscan"], capture_output=True, text=True)
+def determine_profile(vol_engine):
+    profile_output = subprocess.run([vol_engine.vol_loc, vol_engine.memory_file, "kdbgscan"], capture_output=True, text=True)
     print(profile_output)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Autovolatile Stuff')
+    parser.add_argument('-l', '--location', action='store', required=True)
     parser.add_argument('-f', '--file', action='store')
+    parser.add_argument('-p', '--profile', action='store')
     args = parser.parse_args()
 
-
-
-
-
-
-
-
-
-
-    #profile = "--profile " + profile_found
-
-
-
-
-
-
-
+    vol_engine = Vol_Bot(args.profile, args.file, args.location)
+    
+    if vol_engine.profile == None:
+        determine_profile(vol_engine)
 
 
 
---
+++ vol_bot.py
@@ -0,0 +1,26 @@
+
+
+
+class Vol_Bot:
+
+
+    def __init__(self, profile, memory_file, vol_loc):
+        self.profile = profile
+        self.memory_file = memory_file
+        self.vol_loc = vol_loc
+
+
+
+    def set_memory_file(self, memory_file):
+        self.memory_file = memory_file
+
+
+
+    def set_profile(self, profile):
+        self.profile = profile
+
+
+    def set_vol_loc(self, filepath):
+        self.vol_loc = filepath
+
+
